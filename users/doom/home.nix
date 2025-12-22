bca1383e2f4fa4a32e28d04ea5966bb867f506f8
Roman Shchekotov <shch@cs.tum.edu>
2025-12-22 16:59:17 +0100

    fix: add the uid

--- users/doom/home.nix
+++ users/doom/home.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   home.username = "doom";
+  home.uid = 1000;
   home.homeDirectory = "/home/doom";
   home.stateVersion = "25.11";
   home.enableNixpkgsReleaseCheck = false;
