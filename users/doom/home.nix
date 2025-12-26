ec27b81c8ccb84b482db4129a0127821a65aade3
Roman Shchekotov <shch@cs.tum.edu>
2025-12-26 16:55:03 +0100

    fix: remove tailscale from HM for root's sake

--- users/doom/home.nix
+++ users/doom/home.nix
@@ -4,7 +4,7 @@
   home.homeDirectory = "/home/doom";
   home.stateVersion = "25.11";
   home.enableNixpkgsReleaseCheck = false;
-  home.packages = with pkgs; [ lsof tailscale ];
+  home.packages = with pkgs; [ lsof ];
   home.file = {};
   home.sessionVariables = {};
   programs.home-manager.enable = true;
